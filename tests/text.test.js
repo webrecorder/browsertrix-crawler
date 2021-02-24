56e21684a274db85c620cd6bd678f0065f7d49f6
Emma Dickson <emmadickson@Emmas-MacBook-Pro.local>
2021-02-24 17:40:22 -0500

    update test paths

--- tests/text.test.js
+++ tests/text.test.js
@@ -3,26 +3,20 @@ const md5 = require('md5');
 
 
 test('check that the pages.jsonl file exists in the collection under the pages folder', () => {
-  expect(fs.existsSync('collections/wr-net/pages/pages.jsonl')).toBe(true);
+  expect(fs.existsSync('crawls/collections/wr-net/pages/pages.jsonl')).toBe(true);
 });
 
 test('check that the pages.jsonl file exists in the wacz under the pages folder', () => {
-  expect(fs.existsSync('collections/wr-net/wacz/pages/pages.jsonl')).toBe(true);
+  expect(fs.existsSync('crawls/collections/wr-net/wacz/pages/pages.jsonl')).toBe(true);
 });
 
 test('check that the hash in the pages folder and in the unzipped wacz folders match', () => {
-  const browsertrix_hash = md5(fs.readFileSync('collections/wr-net/wacz/pages/pages.jsonl', 'utf8'))
-  const wacz_hash = md5(fs.readFileSync('collections/wr-net/wacz/pages/pages.jsonl', 'utf8'))
-
-  expect(browsertrix_hash).toEqual(wacz_hash);
-});
-
-test('check that the fixture hash matches both of the other hashes', () => {
-   const fixture_hash = md5(fs.readFileSync('tests/fixtures/pages.jsonl', 'utf8'))
-   const browsertrix_hash = md5(fs.readFileSync('collections/wr-net/wacz/pages/pages.jsonl', 'utf8'))
-   const wacz_hash = md5(fs.readFileSync('collections/wr-net/wacz/pages/pages.jsonl', 'utf8'))
-
-  expect(browsertrix_hash).toEqual(wacz_hash);
-  expect(browsertrix_hash).toEqual(fixture_hash);
+  const browsertrix_hash = md5(JSON.parse(fs.readFileSync('crawls/collections/wr-net/wacz/pages/pages.jsonl', 'utf8').split('\n')[1])['text']);
+  const wacz_hash = md5(JSON.parse(fs.readFileSync('crawls/collections/wr-net/pages/pages.jsonl', 'utf8').split('\n')[1])['text']);
+  const fixture_hash = md5(JSON.parse(fs.readFileSync('tests/fixtures/pages.jsonl', 'utf8').split('\n')[1])['text']);
+  
   expect(wacz_hash).toEqual(fixture_hash);
+  expect(wacz_hash).toEqual(browsertrix_hash);
+
 });
+
