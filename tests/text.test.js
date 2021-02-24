879ef7c81d0d8848aece009df3fc81d19531b3f9
Emma Dickson <emmadickson@Emmas-MacBook-Pro.local>
2021-02-23 22:16:20 -0500

    update tests

--- tests/text.test.js
+++ tests/text.test.js
@@ -3,41 +3,26 @@ const md5 = require('md5');
 
 
 test('check that the pages.jsonl file exists in the collection under the pages folder', () => {
-  expect(fs.existsSync('crawls/collections/wr-net/pages/pages.jsonl')).toBe(true);
+  expect(fs.existsSync('collections/wr-net/pages/pages.jsonl')).toBe(true);
 });
 
 test('check that the pages.jsonl file exists in the wacz under the pages folder', () => {
-  expect(fs.existsSync('crawls/collections/wr-net/wacz/pages/pages.jsonl')).toBe(true);
+  expect(fs.existsSync('collections/wr-net/wacz/pages/pages.jsonl')).toBe(true);
 });
 
 test('check that the hash in the pages folder and in the unzipped wacz folders match', () => {
-  
-  fs.readFile('crawls/collections/wr-net/wacz/pages/pages.jsonl', function(err, buf) {
-    var browsertrix_hash = md5(buf);
-  });
-  
-  fs.readFile('crawls/collections/wr-net/wacz/pages/pages.jsonl', function(err, buf) {
-    var wacz_hash = md5(buf);
-  });
-  
-  expect(browsertrix_hash).toBe(wacz_hash);
+  const browsertrix_hash = md5(fs.readFileSync('collections/wr-net/wacz/pages/pages.jsonl', 'utf8'))
+  const wacz_hash = md5(fs.readFileSync('collections/wr-net/wacz/pages/pages.jsonl', 'utf8'))
+
+  expect(browsertrix_hash).toEqual(wacz_hash);
 });
 
 test('check that the fixture hash matches both of the other hashes', () => {
-  
-  fs.readFile('tests/fixtures/pages.jsonl', function(err, buf) {
-    var fixture_hash = md5(buf);
-  });
-  
-  fs.readFile('crawls/collections/wr-net/wacz/pages/pages.jsonl', function(err, buf) {
-    var browsertrix_hash = md5(buf);
-  });
-  
-  fs.readFile('crawls/collections/wr-net/wacz/pages/pages.jsonl', function(err, buf) {
-    var wacz_hash = md5(buf);
-  });
-  
-  expect(browsertrix_hash).toBe(wacz_hash);
-  expect(browsertrix_hash).toBe(fixture_hash);
-  expect(wacz_hash).toBe(fixture_hash);
+   const fixture_hash = md5(fs.readFileSync('tests/fixtures/pages.jsonl', 'utf8'))
+   const browsertrix_hash = md5(fs.readFileSync('collections/wr-net/wacz/pages/pages.jsonl', 'utf8'))
+   const wacz_hash = md5(fs.readFileSync('collections/wr-net/wacz/pages/pages.jsonl', 'utf8'))
+
+  expect(browsertrix_hash).toEqual(wacz_hash);
+  expect(browsertrix_hash).toEqual(fixture_hash);
+  expect(wacz_hash).toEqual(fixture_hash);
 });
