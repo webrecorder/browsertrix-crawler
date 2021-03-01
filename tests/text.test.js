16fcd97bb739cf73b7aaea090269804e21270a74
Emma Dickson <emmadickson@Emmas-MacBook-Pro.local>
2021-03-01 14:21:45 -0500

    update

--- tests/text.test.js
+++ tests/text.test.js
@@ -11,12 +11,12 @@ test('check that the pages.jsonl file exists in the wacz under the pages folder'
 });
 
 test('check that the hash in the pages folder and in the unzipped wacz folders match', () => {
-  const browsertrix_hash = md5(JSON.parse(fs.readFileSync('crawls/collections/wr-net/wacz/pages/pages.jsonl', 'utf8').split('\n')[1])['text']);
+  const crawl_hash = md5(JSON.parse(fs.readFileSync('crawls/collections/wr-net/wacz/pages/pages.jsonl', 'utf8').split('\n')[1])['text']);
   const wacz_hash = md5(JSON.parse(fs.readFileSync('crawls/collections/wr-net/pages/pages.jsonl', 'utf8').split('\n')[1])['text']);
   const fixture_hash = md5(JSON.parse(fs.readFileSync('tests/fixtures/pages.jsonl', 'utf8').split('\n')[1])['text']);
   
   expect(wacz_hash).toEqual(fixture_hash);
-  expect(wacz_hash).toEqual(browsertrix_hash);
+  expect(wacz_hash).toEqual(crawls_hash);
 
 });
 
