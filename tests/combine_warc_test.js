f75833068fa54b395cc4e65f7ca0342146f70a10
Emma Dickson <emmadickson@Emmas-MacBook-Pro.local>
2021-03-28 19:09:27 -0400

    add combine warc test

---
+++ tests/combine_warc_test.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+
+test('check that a combined warc file exists in the archive folder', () => {
+  const warcLists = fs.readdirSync(path.join('crawls/collections/wr-net/wacz', 'archive'));
+  var captureFound = 0;
+  
+  for (var i = 0; i < warcLists.length; i++) {
+    if ('capture' in warcLists[i]){
+      captureFound = 1;
+    }
+  }
+
+  expect(captureFound).toEqual(1);
+});
