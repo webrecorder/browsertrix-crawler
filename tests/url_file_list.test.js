cbd8e072f7350e762d20c9ed8ea5379c978de8be
Emma Dickson <emmadickson@Emmas-MacBook-Pro.local>
2021-04-19 18:07:56 -0400

    test updates

--- tests/url_file_list.test.js
+++ tests/url_file_list.test.js
@@ -19,9 +19,7 @@ test("check that all urls in a file list are crawled when the filelisturl param 
   let seed_file = fs.readFileSync("tests/fixtures/urlSeedFile.txt", "utf8").split("\n").sort();
 
   let seed_file_list = [];
-  console.log(seed_file);
   for (var j = 0; j < seed_file.length; j++) {
-    console.log(seed_file[j]);
     if (seed_file[j] != undefined){
       seed_file_list.push(seed_file[j]);
     }
