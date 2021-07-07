454c57ead3b7b1e07cf9bd3e6f2a2b0b5f7820cc
Ilya Kreymer <ikreymer@gmail.com>
2021-07-06 23:50:05 -0700

    fix tests, use builtin zlib.gunzip, revert package.json changes

--- tests/warcinfo.test.js
+++ tests/warcinfo.test.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const gunzip = require("gunzip-file");
+const zlib = require("zlib");
 const child_process = require("child_process");
 
 test("check that the warcinfo file works as expected on the command line", async () => {
@@ -8,7 +8,7 @@ test("check that the warcinfo file works as expected on the command line", async
   try{
     const configYaml = fs.readFileSync("tests/fixtures/crawl-2.yaml", "utf8");
     const version = require("../package.json").version;
-    const proc = child_process.execSync(`docker run -i -v $PWD/crawls:/crawls webrecorder/browsertrix-crawler:${version} crawl --config stdin --exclude webrecorder.net/202`, {input: configYaml, stdin: "inherit", encoding: "utf8"});
+    const proc = child_process.execSync(`docker run -i -v $PWD/crawls:/crawls webrecorder/browsertrix-crawler:${version} crawl --config stdin --limit 1 --collection warcinfo --combineWARC`, {input: configYaml, stdin: "inherit", encoding: "utf8"});
 
     console.log(proc);
   }
@@ -16,11 +16,13 @@ test("check that the warcinfo file works as expected on the command line", async
     console.log(error);
   }
 
-  await gunzip("crawls/collections/warcinfo/warcinfo_0.warc.gz", "crawls/collections/warcinfo/warcinfo_0.warc", () => {
-    var data = fs.readFileSync("crawls/collections/warcinfo/warcinfo_0.warc", "utf-8");
-    var foundWarc = data.indexOf("operator");
-    expect(foundWarc).toBeGreaterThan(-1);
+  const warcData = fs.readFileSync("crawls/collections/warcinfo/warcinfo_0.warc.gz");
 
-  });
+  const data = zlib.gunzipSync(warcData);
+
+  const string = data.toString("utf8");
+
+  expect(string.indexOf("operator: test")).toBeGreaterThan(-1);
+  expect(string.indexOf("host: hostname")).toBeGreaterThan(-1);
 
 });
