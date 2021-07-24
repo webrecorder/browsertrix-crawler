819173679319ddeb513efe83c6b364938475c078
Ilya Kreymer <ikreymer@gmail.com>
2021-07-23 17:20:57 -0700

    tests: fix custom driver

--- tests/fixtures/driver-1.js
+++ tests/fixtures/driver-1.js
@@ -1,18 +1,4 @@
 module.exports = async ({data, page, crawler}) => {
-  await crawler.loadPage(page, data, {selector: "script[src]", extract: "src", isAttribute: false});
+  await crawler.loadPage(page, data, [{selector: "script[src]", extract: "src", isAttribute: false}]);
 };
 
-/*
-module.exports = async ({data, page, crawler}) => {
-  await crawler.loadPage(page, data);
-  const {seedId, depth} = data;
-
-  const links = await crawler.extractLinks(page, {selector: "figure.file", extract: "data-url", isAttribute: true});
-
-  for (const link of links) {
-    console.log("got link " + link);
-    // allow filtering any links here as needed
-    crawler.queueUrl(seedId, link, depth + 1);
-  }
-};
-*/
