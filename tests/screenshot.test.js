bdaa3cd1e8bde187a329a283f22abcbad5c2243e
Emma Dickson <emmadickson@Emmas-MacBook-Air.local>
2021-10-30 00:31:47 -0400

    pass tests

--- tests/screenshot.test.js
+++ tests/screenshot.test.js
@@ -1,12 +1,12 @@
-const fs = require("fs");
 const child_process = require("child_process");
+const fs = require("fs");
 
-test("check that when the screenshot flag is set a screenshot warc is produced", async () => {
-  child_process.execSync("docker run -v $PWD/test-crawls:/crawls webrecorder/browsertrix-crawler crawl --url http://www.example.com/ --screenshot  --timeout 10000 --rolloverSize 10000 --workers 2");
+test("ensure basic crawl run with screenshot run passes", async () => {
+  child_process.execSync("docker run -v $PWD/test-crawls:/crawls webrecorder/browsertrix-crawler crawl --collection test --url http://www.example.com/ --screenshot --workers 2");
 });
 
-test("check that a screenshotwarc file exists in the archive folder", () => {
-  const warcLists = fs.readdirSync("test-crawls/collections/wr-net/screenshots");
+test("check that a screenshots warc file exists in the archive folder", () => {
+  const warcLists = fs.readdirSync("test-crawls/collections/test/screenshots/");
   var captureFound = 0;
 
   for (var i = 0; i < warcLists.length; i++) {
