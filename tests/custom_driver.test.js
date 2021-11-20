d23b423becbdcafc10793baf8772bccf1152ac07
Ilya Kreymer <ikreymer@gmail.com>
2021-11-20 13:51:59 -0800

    fix tests?

--- tests/custom_driver.test.js
+++ tests/custom_driver.test.js
@@ -22,10 +22,12 @@ test("ensure custom driver with custom selector crawls JS files as pages", async
     pages.add(url);
   }
 
+  console.log(pages);
+
   const expectedPages = new Set([
     "https://www.iana.org/",
-    "https://www.iana.org/_js/2013.1/jquery.js",
-    "https://www.iana.org/_js/2013.1/iana.js"
+    "https://www.iana.org/_js/jquery.js",
+    "https://www.iana.org/_js/iana.js"
   ]);
 
   expect(pages).toEqual(expectedPages);
