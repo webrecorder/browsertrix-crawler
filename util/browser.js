2a612359c94737cc57feb9f07ac391e561fbd496
Ilya Kreymer <ikreymer@gmail.com>
2022-03-14 13:53:10 -0700

    add request package

--- util/browser.js
+++ util/browser.js
@@ -2,6 +2,7 @@ const child_process = require("child_process");
 const fs = require("fs");
 const path = require("path");
 const os = require("os");
+const request = require("request");
 
 const profileDir = fs.mkdtempSync(path.join(os.tmpdir(), "profile-"));
 
