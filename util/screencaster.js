d351d1b27dc1d2252cfc524f324c9b2c9081dc74
Ilya Kreymer <ikreymer@gmail.com>
2022-08-18 23:59:50 +0000

    new window tweaks: add reuseCount, use browser.target() instead of opening a new blank page

--- util/screencaster.js
+++ util/screencaster.js
@@ -305,53 +305,56 @@ class NewWindowPage extends SingleBrowserImplementation {
     await super.init();
 
     this.pendingTargets = new Map();
-
-    this.mainPage = await this.browser.newPage();
+    this.startPage = "about:blank?_browsertrix" + Math.random().toString(36).slice(2);
 
     this.pages = [];
-    this.reuse = true;
+    this.reuseCount = 25;
 
-    this.storeTarget = 0;
-
-    //await this.mainPage.goto("about:blank");
-
-    this.mainTarget = this.mainPage.target();
-    this.cdp = await this.mainTarget.createCDPSession();
+    const mainTarget = this.browser.target();
+    this.cdp = await mainTarget.createCDPSession();
 
     this.browser.on("targetcreated", (target) => {
-      if (this.storeTarget && target.url() === "about:blank") {
-        console.log("target created", target._targetId, target);
+      if (target.url() === this.startPage) {
         this.pendingTargets.set(target._targetId, target);
       }
     });
   }
 
   async getNewPage() {
-    try {
-      this.storeTarget++;
-      const {targetId} = await this.cdp.send("Target.createTarget", {url: "about:blank", newWindow: true});
-      this.storeTarget--;
+    while (true) {
+      let targetId;
+      try {
+        const res = await this.cdp.send("Target.createTarget", {url: this.startPage, newWindow: true});
+        targetId = res.targetId;
+      } catch (e) {
+        console.warn(e);
+        return null;
+      }
 
       const target = this.pendingTargets.get(targetId);
+      // this shouldn't really happen, but just in case somehow ended up w/o a target, try again
+      if (!target) {
+        continue;
+      }
+
       this.pendingTargets.delete(targetId);
 
-      return {page: await target.page() };
-    } catch (e) {
-      console.log(e);
+      return {page: await target.page(), count: 0};
     }
   }
 
   async createResources() {
     if (this.pages.length) {
-      return {page: this.pages.shift()};
+      return this.pages.shift();
     }
     return await this.getNewPage();
   }
 
   async freeResources(resources) {
-    if (this.reuse) {
-      this.pages.push(resources.page);
+    if (++resources.count <= this.reuseCount) {
+      this.pages.push(resources);
     } else {
+      //console.log(`page not reused, ${this.reuseCount} reached`);
       await resources.page.close();
     }
   }
