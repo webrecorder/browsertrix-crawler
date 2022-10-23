faf2318fdc6e4f7e787e90941dd8a40fec3bc956
Ed Summers <ehs@pobox.com>
2022-10-23 12:24:52 -0400

    Check if group/user exists
    Ensure that group and user do not already exist before creating them.
    
    Fixes #174

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
 
-set -e
-
 # Get UID/GID from volume dir
+
 VOLUME_UID=$(stat -c '%u' /crawls)
 VOLUME_GID=$(stat -c '%g' /crawls)
+
+# Get the UID/GID we are running as
 
 MY_UID=$(id -u)
 MY_GID=$(id -g)
 
-# Run as custom user
+# If we aren't running as the owner of the /crawls/ dir then add a new user
+# btrix with the same UID/GID of the /crawls dir and run as that user instead.
+
 if [ "$MY_GID" != "$VOLUME_GID" ] || [ "$MY_UID" != "$VOLUME_UID" ]; then
-    # create or modify user and group to match expected uid/gid
-    groupadd --gid $VOLUME_GID archivist || groupmod -o --gid $VOLUME_GID archivist
-    useradd -ms /bin/bash -u $VOLUME_UID -g $VOLUME_GID archivist || usermod -o -u $VOLUME_UID archivist
+    groupadd btrix
+    groupmod -o --gid $VOLUME_GID btrix
+
+    useradd -ms /bin/bash -g $VOLUME_GID btrix
+    usermod -o -u $VOLUME_UID btrix > /dev/null
 
     cmd="cd $PWD; $@"
-
-    # run process as new archivist user
-    su archivist -c "$cmd"
-
-# run as current user (root)
+    su btrix -c "$cmd"
 else
     exec $@
 fi
