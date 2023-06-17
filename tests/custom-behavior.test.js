b687543ad8dc52b51c07c74676361ccf0bab678a
Ilya Kreymer <ikreymer@gmail.com>
2023-06-16 21:03:33 -0700

    lint fix

--- tests/custom-behavior.test.js
+++ tests/custom-behavior.test.js
@@ -1,5 +1,4 @@
 import child_process from "child_process";
-import fs from "fs";
 
 test("test custom behaviors", async () => {
   const res = child_process.execSync("docker run -v $PWD/test-crawls:/crawls -v $PWD/tests/custom-behaviors/:/custom-behaviors/ webrecorder/browsertrix-crawler crawl --url https://example.com/ --url https://example.org/ --customBehaviors /custom-behaviors/");
@@ -7,12 +6,12 @@ test("test custom behaviors", async () => {
   const log = res.toString();
 
   // custom behavior ran for example.com
-  expect(log.indexOf(`{"state":{},"msg":"test-stat","page":"https://example.com/","workerid":0}}`) > 0).toBe(true);
+  expect(log.indexOf("{\"state\":{},\"msg\":\"test-stat\",\"page\":\"https://example.com/\",\"workerid\":0}}") > 0).toBe(true);
 
   // but not for example.org
-  expect(log.indexOf(`{"state":{},"msg":"test-stat","page":"https://example.org/","workerid":0}}`) > 0).toBe(false);
+  expect(log.indexOf("{\"state\":{},\"msg\":\"test-stat\",\"page\":\"https://example.org/\",\"workerid\":0}}") > 0).toBe(false);
 
-  expect(log.indexOf(`{"state":{"segments":1},"msg":"Skipping autoscroll, page seems to not be responsive to scrolling events","page":"https://example.org/","workerid":0}}`) > 0).toBe(true);
+  expect(log.indexOf("{\"state\":{\"segments\":1},\"msg\":\"Skipping autoscroll, page seems to not be responsive to scrolling events\",\"page\":\"https://example.org/\",\"workerid\":0}}") > 0).toBe(true);
 
 });
 
