d99c3fb7a3576655fb04f21ac9e4753a28893ff6
Ilya Kreymer <ikreymer@gmail.com>
2023-06-16 20:47:15 -0700

    tests: add tests for running custom behavior via --customBehaviors and matching specific scope

---
+++ tests/custom-behavior.test.js
@@ -0,0 +1,19 @@
+import child_process from "child_process";
+import fs from "fs";
+
+test("test custom behaviors", async () => {
+  const res = child_process.execSync("docker run -v $PWD/test-crawls:/crawls -v $PWD/tests/custom-behaviors/:/custom-behaviors/ webrecorder/browsertrix-crawler crawl --url https://example.com/ --url https://example.org/ --customBehaviors /custom-behaviors/");
+
+  const log = res.toString();
+
+  // custom behavior ran for example.com
+  expect(log.indexOf(`{"state":{},"msg":"test-stat","page":"https://example.com/","workerid":0}}`) > 0).toBe(true);
+
+  // but not for example.org
+  expect(log.indexOf(`{"state":{},"msg":"test-stat","page":"https://example.org/","workerid":0}}`) > 0).toBe(false);
+
+  expect(log.indexOf(`{"state":{"segments":1},"msg":"Skipping autoscroll, page seems to not be responsive to scrolling events","page":"https://example.org/","workerid":0}}`) > 0).toBe(true);
+
+});
+
+
