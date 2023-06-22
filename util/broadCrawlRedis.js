83185637788260ff65921e20076fe66fea74733e
Aditya Raj <aditya.raj@synaptic.com>
2023-06-22 18:38:06 +0530

    update yarn file

--- util/broadCrawlRedis.js
+++ util/broadCrawlRedis.js
@@ -2,8 +2,8 @@ import Redis from "ioredis";
 
 export async function initBroadCrawlRedis() {
   const board_crawl_redis = new Redis({
-    host: process.env.BROAD_CRAWL_REDIS_HOST,
-    port: process.env.BROAD_CRAWL_REDIS_PORT
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
   });
   await board_crawl_redis.connect();
   return board_crawl_redis;
