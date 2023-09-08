ecc280e7aa5cd50d9d2588a6c049673f5ef8f661
Anish Lakhwara <anish+git@lakhwara.com>
2023-09-08 11:00:26 -0700

    misc: rename advanced_test to multi_url test

--- tests/mult_url_crawl_with_favicon.test.js
+++ tests/mult_url_crawl_with_favicon.test.js
@@ -1,7 +1,7 @@
 import child_process from "child_process";
 import fs from "fs";
 
-test("ensure advanced crawl run with docker run passes", async () => {
+test("ensure multi url crawl run with docker run passes", async () => {
   child_process.execSync("docker run -v $PWD/test-crawls:/crawls webrecorder/browsertrix-crawler crawl --url https://www.iana.org/ --url https://webrecorder.net/ --generateWACZ --text --collection advanced --combineWARC --rolloverSize 10000 --workers 2 --title \"test title\" --description \"test description\" --pages 2 --limit 2");
 
   child_process.execSync("docker run -v $PWD/test-crawls:/crawls webrecorder/browsertrix-crawler wacz validate --file collections/advanced/advanced.wacz");
