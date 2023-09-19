8e06b1fd930731f51148f01e986daf6501d773b0
Anish Lakhwara <anish+git@lakhwara.com>
2023-09-19 16:10:26 -0700

    fix: tests

--- tests/url-behavior.test.js
+++ tests/url-behavior.test.js
@@ -1,9 +1,9 @@
 import child_process from "child_process";
 
 test("test custom behaviors", async () => {
-  const res = child_process.execSync("docker run -v $PWD/test-crawls:/crawls webrecorder/browsertrix-crawler crawl --url --url https://webrecorder.net/ --customBehaviors 'https://raw.githubusercontent.com/webrecorder/browsertrix-behaviors/main/dist/behaviors.js' --scopeType page");
+  const res = child_process.execSync("docker run -v $PWD/test-crawls:/crawls webrecorder/browsertrix-crawler crawl --url --url https://webrecorder.net/ --customBehaviors 'https://raw.githubusercontent.com/webrecorder/browsertrix-behaviors/main/dist/behaviors.js' --scopeType page --behaviors \"\"");
 
   const log = res.toString();
 
-  expect(log.indexOf("{\"state\":{\"segments\":1},\"msg\":\"Skipping autoscroll, page seems to not be responsive to scrolling events\",\"page\":\"https://webrecorder.net/\",\"workerid\":0}") > 0).toBe(true);
+  expect(log.indexOf("File downloaded to /app/behaviors/") > 0).toBe(true);
 });
