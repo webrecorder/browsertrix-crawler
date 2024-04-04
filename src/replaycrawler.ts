f81b6f29605119e9f1510df0d8e263a39d695507
Ilya Kreymer <ikreymer@gmail.com>
2024-04-04 12:15:32 -0700

    tweak error message

--- src/replaycrawler.ts
+++ src/replaycrawler.ts
@@ -329,7 +329,7 @@ export class ReplayCrawler extends Crawler {
             try {
               frame.evaluate("window.location.reload();");
             } catch (e) {
-              logger.error("RWP Reload failed, retrying", e, "replay");
+              logger.error("RWP Reload failed", e, "replay");
             }
           }, 10000);
           this.reloadTimeouts.set(page, timeoutid);
