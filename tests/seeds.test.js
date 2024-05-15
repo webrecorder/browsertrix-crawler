f529bdccdc2c7e06b123158dd2af0e12f481de1b
Tessa Walsh <tessa@bitarchivist.net>
2024-05-14 22:45:28 -0400

    Adjust no valid seeds test

--- tests/seeds.test.js
+++ tests/seeds.test.js
@@ -42,11 +42,11 @@ test("ensure seed with network error fails crawl if failOnFailedSeed and failOnI
   expect(passed).toBe(false);
 });
 
-test("ensure seed with 4xx/5xx response fails crawl if failOnFailedSeed and failOnInvalidStatus is set", async () => {
+test("ensure seed with 4xx/5xx response fails crawl if failOnFailedSeed and failOnInvalidStatus are set", async () => {
   let passed = true;
   try {
     await exec(
-      "docker run -v $PWD/test-crawls:/crawls webrecorder/browsertrix-crawler crawl --url https://www.iana.org/ --url https://webrecorder.net/404 --generateWACZ --limit 2 --failOnFailedSeed --failOnInvalidStatus --collection failseedstatus",
+      "docker run -v $PWD/test-crawls:/crawls webrecorder/browsertrix-crawler crawl --url https://www.iana.org/ --url https://webrecorder.net/404 --generateWACZ --limit 2 --failOnFailedSeed --failOnInvalidStatus --collection failseed404status",
     );
   } catch (error) {
     expect(error.code).toEqual(1);
@@ -75,7 +75,7 @@ test("ensure crawl fails if no valid seeds are passed", async () => {
       "docker run -v $PWD/test-crawls:/crawls webrecorder/browsertrix-crawler crawl --url iana.org/ --url example.invalid --generateWACZ --limit 2 --collection allinvalidseeds",
     );
   } catch (error) {
-    expect(error.code).toEqual(1);
+    expect(error.code).toEqual(17);
     passed = false;
   }
   expect(passed).toBe(false);
