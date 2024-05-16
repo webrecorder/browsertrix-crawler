a8f8ee4895a14be62ccc1e3e91acf67ac49d9faa
Ed Summers <ehs@pobox.com>
2024-05-16 14:15:30 -0400

    trying to use a local webserver

--- tests/http_basicauth.test.js
+++ tests/http_basicauth.test.js
@@ -1,15 +1,29 @@
 import child_process from "child_process";
 import fs from "fs";
+import http from 'http';
 
-test("test that http basic auth works", async () => {
-  child_process.execSync(
-    'docker run -v $PWD/test-crawls:/crawls webrecorder/browsertrix-crawler crawl --url http://httpbin.org/basic-auth/foo/bar --httpBasicAuth foo:bar --collection basicauth-test --statsFilename stats.json',
-  );
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.write("Hi!");
+  res.end();
 });
 
-test("check that the page didn't fail", () => {
+beforeAll(() => {
+  server.listen(9998, '0.0.0.0');
+});
+
+test("test that http basic auth works", async () => {
+
+  child_process.execSync(
+    "docker run -v $PWD/test-crawls:/crawls webrecorder/browsertrix-crawler crawl --url http://host.docker.internal:9998/ --httpBasicAuth foo:bar --collection basicauth-test --statsFilename stats.json",
+  );
+
   const data = fs.readFileSync("test-crawls/stats.json", "utf8");
   const dataJSON = JSON.parse(data);
   expect(dataJSON.crawled).toEqual(1);
   expect(dataJSON.failed).toEqual(0);
 });
+
+afterAll(() => {
+  server.close();
+})
