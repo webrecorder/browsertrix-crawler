a90ceb48178809aff8cee6ca1cff4b30298507c8
Tessa Walsh <tessa@bitarchivist.net>
2024-05-17 11:40:33 -0400

    Remove trailing slash after port in seed url

--- tests/http_basicauth.test.js
+++ tests/http_basicauth.test.js
@@ -15,7 +15,7 @@ beforeAll(() => {
 test("test that http basic auth works", async () => {
 
   child_process.execSync(
-    "docker run -v $PWD/test-crawls:/crawls webrecorder/browsertrix-crawler crawl --url http://host.docker.internal:9998/ --httpBasicAuth foo:bar --collection basicauth-test --statsFilename stats.json",
+    "docker run -v $PWD/test-crawls:/crawls webrecorder/browsertrix-crawler crawl --url http://host.docker.internal:9998 --httpBasicAuth foo:bar --collection basicauth-test --statsFilename stats.json",
   );
 
   const data = fs.readFileSync("test-crawls/stats.json", "utf8");
