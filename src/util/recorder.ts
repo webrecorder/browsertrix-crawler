8dbc2bb7aef4ede9b42c5dce8ea743b9eab1e71c
Ilya Kreymer <ikreymer@gmail.com>
2024-06-12 11:35:34 -0700

    page load timeout: consolidate Page Load Failed (loadState === 0) and Page Load Timeout (loadState === 1) messages change streaming fetch stack trace to debug

--- src/util/recorder.ts
+++ src/util/recorder.ts
@@ -1272,7 +1272,7 @@ class AsyncFetcher {
       if (e.message === "response-filtered-out") {
         throw e;
       }
-      logger.warn(
+      logger.debug(
         "Streaming Fetch Error",
         { url, networkId, filename, ...formatErr(e), ...logDetails },
         "recorder",
