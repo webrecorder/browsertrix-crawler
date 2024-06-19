35b02c97c8e58f4cc859ab078d16c5d82c66668c
Ilya Kreymer <ikreymer@gmail.com>
2024-06-18 21:46:55 -0700

    tests: add test to check pages.jsonl + extraPages.jsonl to ensure 2 pages are crawled rename env var

--- tests/http-auth.test.js
+++ tests/http-auth.test.js
@@ -1,9 +1,11 @@
 import { execSync, spawn } from "child_process";
+import fs from "fs";
 
 let proc = null;
 
+const DOCKER_HOST_NAME = "host.docker.internal";
+
 beforeAll(() => {
-  console.log(`Server Host: ${process.env.SERVER_HOST}`);
   proc = spawn("../../node_modules/.bin/http-server", ["-p", "31501", "--username", "user", "--password", "pass"], {cwd: "./docs/site"});
 });
 
@@ -16,7 +18,7 @@ afterAll(() => {
 test("run crawl without auth", () => {
   let status = 0;
   try {
-    execSync(`docker run --rm webrecorder/browsertrix-crawler crawl --url http://${process.env.SERVER_HOST}:31501 --limit 2 --failOnFailedSeed`);
+    execSync(`docker run --rm webrecorder/browsertrix-crawler crawl --url http://${DOCKER_HOST_NAME}:31501 --limit 2 --failOnFailedSeed`);
   } catch (e) {
     status = e.status;
   }
@@ -26,9 +28,29 @@ test("run crawl without auth", () => {
 test("run crawl with auth", () => {
   let status = 0;
   try {
-    execSync(`docker run --rm webrecorder/browsertrix-crawler crawl --url http://user:pass@${process.env.SERVER_HOST}:31501 --limit 2 --failOnFailedSeed`, {stdio: "inherit"});
+    execSync(`docker run --rm -v $PWD/test-crawls:/crawls webrecorder/browsertrix-crawler crawl --url http://user:pass@${DOCKER_HOST_NAME}:31501 --limit 2 --failOnFailedSeed --collection http-auth-test`);
   } catch (e) {
     status = e.status;
   }
+
   expect(status).toBe(0);
+
+  expect(fs
+    .readFileSync(
+      "test-crawls/collections/http-auth-test/pages/pages.jsonl",
+      "utf8",
+    )
+    .trim()
+    .split("\n")
+    .length).toBe(2);
+
+  expect(fs
+    .readFileSync(
+      "test-crawls/collections/http-auth-test/pages/extraPages.jsonl",
+      "utf8",
+    )
+    .trim()
+    .split("\n")
+    .length).toBe(2);
+
 });
