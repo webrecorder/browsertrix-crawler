c5cb93cacc0f5dbf797eff9e7572bfbc5cdea392
Ilya Kreymer <ikreymer@gmail.com>
2024-09-30 15:34:12 -0700

    add test for extraHops beyond depth

--- tests/extra_hops_depth.test.js
+++ tests/extra_hops_depth.test.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 
 import util from "util";
-import { exec as execCallback } from "child_process";
+import { exec as execCallback, execSync } from "child_process";
 
 const exec = util.promisify(execCallback);
 
@@ -69,3 +69,23 @@ test(
   },
   extraHopsTimeout,
 );
+
+
+test("extra hops applies beyond depth limit", () => {
+    try {
+      execSync(
+        "docker run -v $PWD/test-crawls:/crawls -v $PWD/tests/fixtures:/tests/fixtures webrecorder/browsertrix-crawler crawl --collection extra-hops-depth-0 --extraHops 1 --url https://webrecorder.net/ --limit 2 --depth 0 --timeout 10 --exclude community --exclude tools",
+      );
+    } catch (error) {
+      console.log(error);
+    }
+
+    const crawledExtraPages = fs.readFileSync(
+      "test-crawls/collections/extra-hops-depth-0/pages/extraPages.jsonl",
+      "utf8",
+    );
+    const crawledExtraPagesArray = crawledExtraPages.trim().split("\n");
+
+    expect(crawledExtraPagesArray.length - 1).toEqual(1);
+});
+
