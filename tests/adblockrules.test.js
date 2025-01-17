71267c313a6b4db02a39f32e455c4ce0ae428662
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2025-01-17 17:27:23 +0000

    revert changes

--- tests/adblockrules.test.js
+++ tests/adblockrules.test.js
@@ -2,7 +2,6 @@ import child_process from "child_process";
 import fs from "fs";
 import yaml from "js-yaml";
 
-// Function to run the crawl process
 function runCrawl(name, config, commandExtra = "") {
   config.generateCDX = true;
   config.depth = 0;
@@ -11,29 +10,40 @@ function runCrawl(name, config, commandExtra = "") {
   const configYaml = yaml.dump(config);
 
   try {
-    const output = child_process.execSync(
+    const proc = child_process.execSync(
       `docker run -i -v $PWD/test-crawls:/crawls webrecorder/browsertrix-crawler crawl --config stdin ${commandExtra}`,
       { input: configYaml, stdin: "inherit", encoding: "utf8" },
     );
 
-    console.log("Crawl completed successfully:", output);
+    //console.log(proc);
   } catch (error) {
-    console.error("Error during crawl process:", error);
-    throw error;
+    console.log(error);
   }
 }
 
-// Function to check if the CDX file contains a specific value
 function doesCDXContain(coll, value) {
   const data = fs.readFileSync(
     `test-crawls/collections/${coll}/indexes/index.cdxj`,
-    "utf8",
   );
-  return data.includes(value);
+  return data.indexOf(value) >= 0;
 }
 
-// Test case: Crawl with ad block enabled
-test("Test crawl with ad block for specific URL", () => {
+// Test Disabled for Brave -- should always be blocked, but seeing inconsistent ci behavior
+/*
+test("test crawl without ad block for specific URL", () => {
+  const config = {
+    "url": "https://www.mozilla.org/en-US/firefox/",
+    "pageExtraDelay": 10
+  };
+
+  runCrawl("adblock-no-block", config);
+
+  // without ad blocking, URL with googletagmanager is included
+  expect(doesCDXContain("adblock-no-block", "www.googletagmanager.com")).toBe(true);
+});
+*/
+
+test("testcrawl with ad block for specific URL", () => {
   const config = {
     url: "https://www.mozilla.org/en-US/firefox/",
     blockAds: true,
@@ -42,23 +52,6 @@ test("Test crawl with ad block for specific URL", () => {
   runCrawl("adblock-block", config);
 
   expect(doesCDXContain("adblock-block", "www.googletagmanager.com")).toBe(
-    false, // Expect "www.googletagmanager.com" to NOT be included
+    false,
   );
 });
-
-// Commented-out test case: Crawl without ad block
-// Disabled due to inconsistent CI behavior, but left here for future debugging
-/*
-test("Test crawl without ad block for specific URL", () => {
-  const config = {
-    url: "https://www.mozilla.org/en-US/firefox/",
-    pageExtraDelay: 10,
-  };
-
-  runCrawl("adblock-no-block", config);
-
-  expect(doesCDXContain("adblock-no-block", "www.googletagmanager.com")).toBe(
-    true, // Expect "www.googletagmanager.com" to be included
-  );
-});
-*/
