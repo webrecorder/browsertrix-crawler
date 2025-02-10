ff6bfc363327d78897d47670d38bf3ce21041765
Ilya Kreymer <ikreymer@gmail.com>
2025-02-10 13:34:00 -0800

    tweak text to avoid dupe message

--- src/main.ts
+++ src/main.ts
@@ -30,9 +30,7 @@ async function handleTerminate(signame: string) {
     await crawler.checkCanceled();
 
     if (!crawler.interruptReason) {
-      logger.info(
-        "SIGNAL: interrupt request received, finishing current pages before exit...",
-      );
+      logger.info("SIGNAL: interrupt request received...");
       crawler.gracefulFinishOnInterrupt(InterruptReason.SignalInterrupted);
     } else if (forceTerm || Date.now() - lastSigInt > 200) {
       logger.info("SIGNAL: stopping crawl now...");
