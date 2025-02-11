ec414ca7a39b6ac78c43a4229e58c2b2e656887b
Tessa Walsh <tessa@bitarchivist.net>
2025-02-11 14:35:59 -0500

    Use ExitCodes.SignalInterrupted for profile SIGINT/SIGTERM

--- src/create-login-profile.ts
+++ src/create-login-profile.ts
@@ -147,7 +147,7 @@ function getDefaultWindowSize() {
 
 function handleTerminate(signame: string) {
   logger.info(`Got signal ${signame}, exiting`);
-  process.exit(ExitCodes.GenericError);
+  process.exit(ExitCodes.SignalInterrupted);
 }
 
 async function main() {
