024a35fa21c1d25a0704f9c8336be6f00a2de234
Tessa Walsh <tessa@bitarchivist.net>
2025-04-03 12:51:50 -0400

    Fix existing log tests for behaviors 0.8.2 and add redis test

--- tests/custom-behavior.test.js
+++ tests/custom-behavior.test.js
@@ -1,4 +1,11 @@
 import child_process from "child_process";
+import Redis from "ioredis";
+
+
+async function sleep(time) {
+  await new Promise((resolve) => setTimeout(resolve, time));
+}
+
 
 test("test custom behaviors from local filepath", async () => {
   const res = child_process.execSync(
@@ -10,21 +17,21 @@ test("test custom behaviors from local filepath", async () => {
   // custom behavior ran for specs.webrecorder.net
   expect(
     log.indexOf(
-      '"logLevel":"info","context":"behaviorScript","message":"test-stat","details":{"state":{},"behavior":"TestBehavior","page":"https://specs.webrecorder.net/","workerid":0}}',
+      '"logLevel":"info","context":"behaviorScriptCustom","message":"test-stat","details":{"state":{},"behavior":"TestBehavior","page":"https://specs.webrecorder.net/","workerid":0}}',
     ) > 0,
   ).toBe(true);
 
   // but not for example.org
   expect(
     log.indexOf(
-      '"logLevel":"info","context":"behaviorScript","message":"test-stat","details":{"state":{},"behavior":"TestBehavior","page":"https://example.org","workerid":0}}',
+      '"logLevel":"info","context":"behaviorScriptCustom","message":"test-stat","details":{"state":{},"behavior":"TestBehavior","page":"https://example.org","workerid":0}}',
     ) > 0,
   ).toBe(false);
 
   // another custom behavior ran for old.webrecorder.net
   expect(
     log.indexOf(
-      '"logLevel":"info","context":"behaviorScript","message":"test-stat-2","details":{"state":{},"behavior":"TestBehavior2","page":"https://old.webrecorder.net/","workerid":0}}',
+      '"logLevel":"info","context":"behaviorScriptCustom","message":"test-stat-2","details":{"state":{},"behavior":"TestBehavior2","page":"https://old.webrecorder.net/","workerid":0}}',
     ) > 0,
   ).toBe(true);
 });
@@ -38,7 +45,7 @@ test("test custom behavior from URL", async () => {
 
   expect(
     log.indexOf(
-      '"logLevel":"info","context":"behaviorScript","message":"test-stat-2","details":{"state":{},"behavior":"TestBehavior"2,"page":"https://old.webrecorder.net/","workerid":0}}',
+      '"logLevel":"info","context":"behaviorScriptCustom","message":"test-stat-2","details":{"state":{},"behavior":"TestBehavior"2,"page":"https://old.webrecorder.net/","workerid":0}}',
     ) > 0,
   ).toBe(true);
 });
@@ -53,14 +60,14 @@ test("test mixed custom behavior sources", async () => {
 
   expect(
     log.indexOf(
-      '"logLevel":"info","context":"behaviorScript","message":"test-stat","details":{"state":{},"behavior":"TestBehavior","page":"https://specs.webrecorder.net/","workerid":0}}',
+      '"logLevel":"info","context":"behaviorScriptCustom","message":"test-stat","details":{"state":{},"behavior":"TestBehavior","page":"https://specs.webrecorder.net/","workerid":0}}',
     ) > 0,
   ).toBe(true);
 
   // test custom behavior from local file ran
   expect(
     log.indexOf(
-      '"logLevel":"info","context":"behaviorScript","message":"test-stat-2","details":{"state":{},"behavior":"TestBehavior2","page":"https://old.webrecorder.net/","workerid":0}}',
+      '"logLevel":"info","context":"behaviorScriptCustom","message":"test-stat-2","details":{"state":{},"behavior":"TestBehavior2","page":"https://old.webrecorder.net/","workerid":0}}',
     ) > 0,
   ).toBe(true);
 });
@@ -75,21 +82,21 @@ test("test custom behaviors from git repo", async () => {
   // custom behavior ran for specs.webrecorder.net
   expect(
     log.indexOf(
-      '"logLevel":"info","context":"behaviorScript","message":"test-stat","details":{"state":{},"behavior":"TestBehavior","page":"https://specs.webrecorder.net/","workerid":0}}',
+      '"logLevel":"info","context":"behaviorScriptCustom","message":"test-stat","details":{"state":{},"behavior":"TestBehavior","page":"https://specs.webrecorder.net/","workerid":0}}',
     ) > 0,
   ).toBe(true);
 
   // but not for example.org
   expect(
     log.indexOf(
-      '"logLevel":"info","context":"behaviorScript","message":"test-stat","details":{"state":{},"behavior":"TestBehavior","page":"https://example.org/","workerid":0}}',
+      '"logLevel":"info","context":"behaviorScriptCustom","message":"test-stat","details":{"state":{},"behavior":"TestBehavior","page":"https://example.org/","workerid":0}}',
     ) > 0,
   ).toBe(false);
 
   // another custom behavior ran for old.webrecorder.net
   expect(
     log.indexOf(
-      '"logLevel":"info","context":"behaviorScript","message":"test-stat-2","details":{"state":{},"behavior":"TestBehavior2","page":"https://old.webrecorder.net/","workerid":0}}',
+      '"logLevel":"info","context":"behaviorScriptCustom","message":"test-stat-2","details":{"state":{},"behavior":"TestBehavior2","page":"https://old.webrecorder.net/","workerid":0}}',
     ) > 0,
   ).toBe(true);
 });
@@ -152,4 +159,45 @@ test("test crawl exits if not custom behaviors collected from local path", async
 
   // logger fatal exit code
   expect(status).toBe(17);
+});
+
+test("test pushing behavior logs to redis", async () => {
+  const child = child_process.exec("docker run -v $PWD/test-crawls:/crawls -v $PWD/tests/custom-behaviors/:/custom-behaviors/ -e CRAWL_ID=behavior-logs-redis-test -p 36399:6379 --rm webrecorder/browsertrix-crawler crawl --url https://specs.webrecorder.net/ --url https://old.webrecorder.net/ --customBehaviors https://raw.githubusercontent.com/webrecorder/browsertrix-crawler/refs/heads/main/tests/custom-behaviors/custom-2.js --customBehaviors /custom-behaviors/custom.js --scopeType page --logBehaviorsToRedis");
+
+  let resolve = null;
+  const crawlFinished = new Promise(r => resolve = r);
+
+  // detect crawler exit
+  let crawler_exited = false;
+  child.on("exit", function () {
+    crawler_exited = true;
+    resolve();
+  });
+
+  const redis = new Redis("redis://127.0.0.1:36399/0", { lazyConnect: true, retryStrategy: () => null });
+
+  await sleep(3000);
+
+  await redis.connect({ maxRetriesPerRequest: 50 });
+
+  let logLineCount = 0;
+
+  while (!crawler_exited) {
+    const res = await redis.lpop("behavior-logs-redis-test:b");
+    if (!res) {
+      await sleep(100);
+      continue;
+    }
+    const json = JSON.parse(res);
+    expect(json).toHaveProperty("timestamp");
+    expect(json.logLevel).toBe("info")
+    expect(json.context).toBe("behaviorScriptCustom")
+    expect(["test-stat", "test-stat-2"]).toContain(json.message)
+    expect(["TestBehavior", "TestBehavior2"]).toContain(json.details.behavior)
+    expect(["https://specs.webrecorder.net/", "https://old.webrecorder.net/"]).toContain(json.details.page)
+    logLineCount++
+    break;
+  }
+
+  expect(logLineCount).toEqual(2);
 });
