f22ee8fce1c185ee70453eb7ed9ae13374ddcc4c
Ilya Kreymer <ikreymer@gmail.com>
2025-05-09 21:27:17 -0700

    lang code fixes: - validate --lang values, fail immediately with invalid iso-639-1 country code - ignore --lang value when using profile, print warning that profile language takes precedence - fixes #833

--- src/crawler.ts
+++ src/crawler.ts
@@ -591,7 +591,12 @@ export class Crawler {
 
   extraChromeArgs() {
     const args = [];
-    if (this.params.lang) {
+    if (this.params.profile) {
+      logger.warn(
+        "Ignoring --lang option with profile, using language configured in the profile",
+        { lang: this.params.lang },
+      );
+    } else {
       args.push(`--accept-lang=${this.params.lang}`);
     }
     return args;
