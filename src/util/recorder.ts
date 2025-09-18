3a57865a0fadf24d046e8a2c526d70c40a86e23d
Ilya Kreymer <ikreymer@gmail.com>
2025-09-18 02:04:28 -0700

    update to latest warcio (2.4.7) to fix issus when returning payload only size

--- src/util/recorder.ts
+++ src/util/recorder.ts
@@ -1548,7 +1548,7 @@ export class Recorder extends EventEmitter {
     const { logDetails } = this;
     try {
       reqresp.readSize = await serializer.digestRecord({
-        includeHeadersSize: false,
+        returnPayloadOnlySize: true,
       });
       // set truncated field and recompute header buff
       if (reqresp.truncated) {
@@ -1691,7 +1691,7 @@ export class Recorder extends EventEmitter {
       }
     } else {
       reqresp.readSize = await serializer.digestRecord({
-        includeHeadersSize: false,
+        returnPayloadOnlySize: true,
       });
     }
 
