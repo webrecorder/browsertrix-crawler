11ef9b805d0dd011554cf52ba5e2eff1fb07057b
Tessa Walsh <tessa@bitarchivist.net>
2025-10-14 11:55:16 -0400

    Fix logging for sitemap done

--- src/crawler.ts
+++ src/crawler.ts
@@ -2622,7 +2622,7 @@ self.__bx_behaviors.selectMainBehavior();
     }
 
     if (await this.crawlState.isSitemapDone()) {
-      logger.info("Sitemap already processed, skipping", "sitemap");
+      logger.info("Sitemap already processed, skipping", {}, "sitemap");
       return;
     }
 
