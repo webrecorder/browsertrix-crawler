f1db962eebb5fac29d64dd89bf1ee644002411af
Ilya Kreymer <ikreymer@gmail.com>
2025-10-25 09:33:37 -0700

    rename 'dedup' -> 'dedupe' for consistency

--- src/util/recorder.ts
+++ src/util/recorder.ts
@@ -147,7 +147,7 @@ export class Recorder extends EventEmitter {
 
   pageSeed?: ScopedSeed;
   pageSeedDepth = 0;
-  minPageDedupDepth = -1;
+  minPageDedupeDepth = -1;
 
   frameIdToExecId: Map<string, number> | null;
 
@@ -171,7 +171,7 @@ export class Recorder extends EventEmitter {
 
     this.shouldSaveStorage = !!crawler.params.saveStorage;
 
-    this.minPageDedupDepth = crawler.params.minPageDedupDepth;
+    this.minPageDedupeDepth = crawler.params.minPageDedupeDepth;
 
     this.writer = writer;
 
@@ -843,8 +843,8 @@ export class Recorder extends EventEmitter {
     if (
       url === this.pageUrl &&
       reqresp.payload &&
-      this.minPageDedupDepth >= 0 &&
-      this.pageSeedDepth >= this.minPageDedupDepth
+      this.minPageDedupeDepth >= 0 &&
+      this.pageSeedDepth >= this.minPageDedupeDepth
     ) {
       const hash =
         "sha256:" + createHash("sha256").update(reqresp.payload).digest("hex");
