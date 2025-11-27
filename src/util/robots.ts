f0ace6dd390c0d229c21f37e3a4e539c9d33962f
Ilya Kreymer <ikreymer@gmail.com>
2025-11-26 17:00:14 -0800

    remove robots fetch retry: - for now, limit to 10 seconds to fetch robots, if failed, treat same as no robots

--- src/util/robots.ts
+++ src/util/robots.ts
@@ -4,10 +4,13 @@ import robotsParser, { Robot } from "robots-parser";
 import { LogDetails, logger } from "./logger.js";
 import { RedisCrawlState } from "./state.js";
 import { getProxyDispatcher } from "./proxy.js";
-import { sleep } from "./timing.js";
+import { timedRun } from "./timing.js";
 
 let headers: Record<string, string> = {};
 let crawlState: RedisCrawlState | null = null;
+
+// max seconds to wait to fetch robots
+const ROBOTS_FETCH_TIMEOUT = 10;
 
 export function setRobotsConfig(
   _headers: Record<string, string>,
@@ -49,7 +52,14 @@ async function fetchAndParseRobots(
       { url: robotsUrl, ...logDetails },
       "robots",
     );
-    const content = await fetchRobots(robotsUrl);
+    const content = await timedRun(
+      fetchRobots(robotsUrl),
+      ROBOTS_FETCH_TIMEOUT,
+      "Fetching Robots timed out",
+      logDetails,
+      "robots",
+    );
+
     if (content === null) {
       return null;
     }
@@ -78,45 +88,21 @@ async function fetchAndParseRobots(
 }
 
 async function fetchRobots(url: string): Promise<string | null> {
-  while (true) {
-    const resp = await fetch(url, {
-      headers,
-      dispatcher: getProxyDispatcher(url),
-    });
+  const resp = await fetch(url, {
+    headers,
+    dispatcher: getProxyDispatcher(url),
+  });
 
-    if (resp.ok) {
-      return await resp.text();
-    }
-
-    if (resp.status === 429 || resp.status === 503) {
-      const retry = resp.headers.get("retry-after");
-
-      if (retry) {
-        logger.debug(
-          "Robots.txt fetch: Retry after",
-          { url, retrySeconds: retry },
-          "robots",
-        );
-        await sleep(parseInt(retry));
-        continue;
-      }
-
-      logger.debug(
-        "Robots.txt temporarily not fetched, will retry later",
-        { url, status: resp.status },
-        "robots",
-      );
-
-      return null;
-    }
-
-    logger.debug(
-      "Robots.txt invalid, storing empty value",
-      { url, status: resp.status },
-      "robots",
-    );
-
-    // for other status errors, just return empty
-    return "";
+  if (resp.ok) {
+    return await resp.text();
   }
+
+  logger.debug(
+    "Robots.txt invalid, storing empty value",
+    { url, status: resp.status },
+    "robots",
+  );
+
+  // for other status errors, just return empty
+  return "";
 }
