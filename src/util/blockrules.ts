webrecorder/browsertrix-crawler

commit 88277ea5ab13f195294d67d8966e8e8467604713

Replace fetch() with optimized undici request() (#946)
Per https://undici.nodejs.org/#/?id=benchmarks request() is supposed to
be much more performant compared to fetch() with almost the same
interface.

This PR replaces all of the fetch() calls (both using proxy dispatcher
and regular fetch) with undici request()
The migration is fairly simply as shown in
https://undici.nodejs.org/#/?id=migration-guide

The migration eliminates various web streams to node streams
conversions. To support automatic redirects, undici redirect interceptor is used.

Also updates to latest undici (v7)

src/util/blockrules.ts
@@ -4,7 +4,7 @@ import { logger, formatErr } from "./logger.js";
 import { HTTPRequest, Page } from "puppeteer-core";
 import { Browser } from "./browser.js";
 
-import { fetch } from "undici";
+import { request } from "undici";
 import { getProxyDispatcher } from "./proxy.js";
 
 const RULE_TYPES = ["block", "allowOnly"];
@@ -264,7 +264,7 @@ export class BlockRules {
   }
 
   async isTextMatch(
-    request: HTTPRequest,
+    _: HTTPRequest,
     reqUrl: string,
     frameTextMatch: RegExp,
     // TODO: Fix this the next time the file is edited.
@@ -272,10 +272,10 @@ export class BlockRules {
     logDetails: Record<string, any>,
   ) {
     try {
-      const res = await fetch(reqUrl, {
+      const { body } = await request(reqUrl, {
         dispatcher: getProxyDispatcher(reqUrl),
       });
-      const text = await res.text();
+      const text = await body.text();
 
       return !!text.match(frameTextMatch);
     } catch (e) {
@@ -301,7 +301,7 @@ export class BlockRules {
     const body = this.blockErrMsg;
     const putUrl = new URL(this.blockPutUrl);
     putUrl.searchParams.set("url", url);
-    await fetch(putUrl.href, {
+    await request(putUrl.href, {
       method: "PUT",
       headers: { "Content-Type": "text/html" },
       body,

src/util/browser.ts
@@ -2,10 +2,12 @@ import * as child_process from "child_process";
 import fs from "fs";
 import fsp from "node:fs/promises";
 import { pipeline } from "node:stream/promises";
-import { Readable } from "node:stream";
+import assert from "node:assert";
 import crypto from "crypto";
 
 import path from "path";
+
+import { request } from "undici";
 
 import { formatErr, LogContext, logger } from "./logger.js";
 import { getSafeProxyString } from "./proxy.js";
@@ -31,7 +33,6 @@ import puppeteer, {
 } from "puppeteer-core";
 import { Recorder } from "./recorder.js";
 import { timedRun } from "./timing.js";
-import assert from "node:assert";
 import { replaceDir } from "./file_reader.js";
 
 type BtrixChromeOpts = {
@@ -249,13 +250,9 @@ export class Browser {
         "browser",
       );
 
-      const resp = await fetch(profileRemoteSrc);
-      await pipeline(
-        // TODO: Fix this the next time the file is edited.
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        Readable.fromWeb(resp.body as any),
-        fs.createWriteStream(profileLocalSrc),
-      );
+      const resp = await request(profileRemoteSrc);
+
+      await pipeline(resp.body, fs.createWriteStream(profileLocalSrc));
     } else if (profileRemoteSrc && profileRemoteSrc.startsWith("@")) {
       const storage = initStorage();
 

src/util/file_reader.ts
@@ -1,7 +1,7 @@
 import fsp from "fs/promises";
 import path from "path";
 import crypto from "crypto";
-import { fetch } from "undici";
+import { request } from "undici";
 import util from "util";
 import { exec as execCallback } from "child_process";
 
@@ -78,13 +78,13 @@ async function writeUrlContentsToFile(
   }
 
   try {
-    const res = await fetch(url, {
+    const res = await request(url, {
       dispatcher: useProxy ? getProxyDispatcher(url) : undefined,
     });
-    if (!res.ok) {
-      throw new Error(`Invalid response, status: ${res.status}`);
+    if (res.statusCode !== 200) {
+      throw new Error(`Invalid response, status: ${res.statusCode}`);
     }
-    const fileContents = await res.text();
+    const fileContents = await res.body.text();
 
     const filepath = await getTempFile(targetDir, filename);
 

src/util/recorder.ts
@@ -8,7 +8,7 @@ import {
   isRedirectStatus,
 } from "./reqresp.js";
 
-import { fetch, Response } from "undici";
+import { Agent, Dispatcher, interceptors, request } from "undici";
 
 import {
   getCustomRewriter,
@@ -27,6 +27,7 @@ import { getProxyDispatcher } from "./proxy.js";
 import { ScopedSeed } from "./seeds.js";
 import EventEmitter from "events";
 import { DEFAULT_MAX_RETRIES } from "./constants.js";
+import { Readable } from "stream";
 
 const MAX_BROWSER_DEFAULT_FETCH_SIZE = 5_000_000;
 const MAX_TEXT_REWRITE_SIZE = 25_000_000;
@@ -53,6 +54,8 @@ const RW_MIME_TYPES = [
 ];
 
 const encoder = new TextEncoder();
+
+const defaultAgent = new Agent();
 
 // =================================================================
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -1704,8 +1707,7 @@ class AsyncFetcher {
   cdp: CDPSession | null = null;
 
   stream?: string;
-  resp?: Response;
-  abort?: AbortController;
+  body?: Dispatcher.BodyMixin & Readable;
 
   maxFetchSize: number;
 
@@ -1784,15 +1786,16 @@ class AsyncFetcher {
 
   async loadBody() {
     try {
-      const { reqresp, useBrowserNetwork, resp, stream, cdp, recorder } = this;
+      const { reqresp, useBrowserNetwork, body, stream, cdp, recorder } = this;
 
       let iter: AsyncIterable<Uint8Array> | undefined;
       if (reqresp.expectedSize === 0) {
         iter = undefined;
       } else if (stream && useBrowserNetwork && cdp) {
         iter = recorder.takeStreamIter(this.reqresp, cdp, stream);
-      } else if (resp && resp.body) {
-        iter = this.takeReader(resp.body.getReader());
+      } else if (body) {
+        //iter = this.takeReader(resp.body.getReader());
+        iter = this.takeReader(body);
       } else {
         throw new Error("resp body missing");
       }
@@ -1813,10 +1816,9 @@ class AsyncFetcher {
   }
 
   async doCancel() {
-    const { abort } = this;
-    if (abort) {
-      abort.abort();
-      this.abort = undefined;
+    const { body } = this;
+    if (body && !body.destroyed) {
+      body.destroy();
     }
   }
 
@@ -1827,36 +1829,47 @@ class AsyncFetcher {
 
     const headers = reqresp.getRequestHeadersDict();
 
-    let dispatcher = getProxyDispatcher(url);
+    let dispatcher = getProxyDispatcher(url) || defaultAgent;
 
-    if (dispatcher) {
-      dispatcher = dispatcher.compose((dispatch) => {
-        return (opts, handler) => {
-          if (opts.headers) {
-            reqresp.requestHeaders = opts.headers as Record<string, string>;
-          }
-          return dispatch(opts, handler);
-        };
-      });
+    if (!this.manualRedirect) {
+      // match fetch() max redirects if not doing manual redirects
+      // https://fetch.spec.whatwg.org/#http-redirect-fetch
+      const redirector = interceptors.redirect({ maxRedirections: 20 });
+      dispatcher = dispatcher.compose(redirector);
     }
 
-    this.abort = new AbortController();
+    dispatcher = dispatcher.compose((dispatch) => {
+      return (opts, handler) => {
+        if (opts.headers) {
+          // store full actual headers that are sent for the request
+          reqresp.requestHeaders = opts.headers as Record<string, string>;
+        }
+        return dispatch(opts, handler);
+      };
+    });
 
-    const resp = await fetch(url!, {
-      method,
+    const resp = await request(url!, {
+      method: (method || "GET") as Dispatcher.HttpMethod,
       headers,
       body: reqresp.postData || undefined,
-      redirect: this.manualRedirect ? "manual" : "follow",
       dispatcher,
-      signal: this.abort.signal,
     });
+
+    // do nothing
+    resp.body.on("error", () => {});
+
+    for (const [name, value] of Object.entries(resp.headers)) {
+      if (value instanceof Array) {
+        resp.headers[name] = multiValueHeader(name, value);
+      }
+    }
 
     if (
       reqresp.expectedSize < 0 &&
-      resp.headers.get("content-length") &&
-      !resp.headers.get("content-encoding")
+      resp.headers["content-length"] &&
+      !resp.headers["content-encoding"]
     ) {
-      reqresp.expectedSize = Number(resp.headers.get("content-length") || -1);
+      reqresp.expectedSize = Number(resp.headers["content-length"] || -1);
     }
 
     if (reqresp.expectedSize === 0) {
@@ -1868,7 +1881,8 @@ class AsyncFetcher {
     }
 
     reqresp.fillFetchResponse(resp);
-    this.resp = resp;
+    this.body = resp.body;
+    //this.resp = resp;
     return true;
   }
 
@@ -1939,18 +1953,22 @@ class AsyncFetcher {
     return true;
   }
 
-  async *takeReader(reader: ReadableStreamDefaultReader<Uint8Array>) {
+  async *takeReader(reader: Readable) {
     let size = 0;
     try {
-      while (true) {
-        const { value, done } = await reader.read();
-        if (done) {
-          break;
-        }
-
+      for await (const value of reader) {
         size += value.length;
         yield value;
       }
+      // while (true) {
+      //   const { value, done } = await reader.read();
+      //   if (done) {
+      //     break;
+      //   }
+
+      //   size += value.length;
+      //   yield value;
+      // }
     } catch (e) {
       logger.warn(
         "takeReader interrupted",

src/crawler.ts
@@ -74,6 +74,7 @@ import { isHTMLMime, isRedirectStatus } from "./util/reqresp.js";
 import { initProxy } from "./util/proxy.js";
 import { initFlow, nextFlowStep } from "./util/flowbehavior.js";
 import { isDisallowedByRobots, setRobotsConfig } from "./util/robots.js";
+import { request } from "undici";
 
 const btrixBehaviors = fs.readFileSync(
   new URL(
@@ -1069,9 +1070,10 @@ self.__bx_behaviors.selectMainBehavior();
 
   async getFavicon(page: Page, logDetails: LogDetails): Promise<string> {
     try {
-      const resp = await fetch("http://127.0.0.1:9221/json");
-      if (resp.status === 200) {
-        const browserJson = await resp.json();
+      const resp = await request("http://127.0.0.1:9221/json");
+      if (resp.statusCode === 200) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const browserJson = (await resp.body.json()) as any[];
         for (const jsons of browserJson) {
           // eslint-disable-next-line @typescript-eslint/no-explicit-any
           if (jsons.id === (page.target() as any)._targetId) {

src/replaycrawler.ts
@@ -23,6 +23,7 @@ import { MAX_URL_LENGTH } from "./util/reqresp.js";
 import { openAsBlob } from "fs";
 import { WARCWriter } from "./util/warcwriter.js";
 import { parseRx } from "./util/seeds.js";
+import { request } from "undici";
 
 // RWP Replay Prefix
 const REPLAY_PREFIX = "http://localhost:9990/replay/w/replay/";
@@ -182,8 +183,9 @@ export class ReplayCrawler extends Crawler {
         url = URL.createObjectURL(blob);
       }
 
-      const resp = await fetch(url);
-      const json = await resp.json();
+      const resp = await request(url);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const json = (await resp.body.json()) as any;
 
       // if json contains pages, just load them directly
       if (json.pages) {

src/util/originoverride.ts
@@ -2,7 +2,7 @@ import { HTTPRequest, Page } from "puppeteer-core";
 import { formatErr, logger } from "./logger.js";
 import { Browser } from "./browser.js";
 
-import { fetch } from "undici";
+import { request as httpRequest } from "undici";
 import { getProxyDispatcher } from "./proxy.js";
 
 export class OriginOverride {
@@ -46,14 +46,14 @@ export class OriginOverride {
           headers.set("origin", orig.origin);
         }
 
-        const resp = await fetch(newUrl, {
+        const resp = await httpRequest(newUrl, {
           headers,
           dispatcher: getProxyDispatcher(newUrl),
         });
 
-        const body = Buffer.from(await resp.arrayBuffer());
-        const respHeaders = Object.fromEntries(resp.headers);
-        const status = resp.status;
+        const body = Buffer.from(await resp.body.arrayBuffer());
+        const respHeaders = resp.headers;
+        const status = resp.statusCode;
 
         logger.debug(
           "Origin overridden",

src/util/reqresp.ts
@@ -7,7 +7,7 @@ import {
 import { Protocol } from "puppeteer-core";
 import { postToGetUrl } from "warcio";
 import { HTML_TYPES } from "./constants.js";
-import { Response } from "undici";
+import { Dispatcher } from "undici";
 
 const CONTENT_LENGTH = "content-length";
 const CONTENT_RANGE = "content-range";
@@ -211,9 +211,11 @@ export class RequestResponseInfo {
     this.extraOpts.ipType = params.resourceIPAddressSpace;
   }
 
-  fillFetchResponse(response: Response) {
-    this.responseHeaders = Object.fromEntries(response.headers);
-    this.setStatus(response.status, response.statusText);
+  fillFetchResponse(response: Dispatcher.ResponseData) {
+    this.responseHeaders = response.headers as Record<string, string>;
+    this.setStatus(response.statusCode);
+    // this.responseHeaders = Object.fromEntries(response.headers);
+    // this.setStatus(response.status, response.statusText);
   }
 
   fillRequestExtraInfo(

commit 1e327fc351b11e477e9e677c5d9d20f428a817df

ensure redirects are followed for sitemap, robots, other requests converted from fetch() (#955)
Refactor dispatcher apis:
- Use `getProxyDispatcher(withRedirect = true)` to follow redirects by
default, with option to disable (eg. in recorder). This dispatcher also
ignores TLS errors, to match current browser config. Used for fetching
archival content.
- Use `getFollowRedirectsDispatcher()` with follows redirects but does
not ignore TLS and does not use proxies, for fetching non-archival
configs (profiles, behaviors, etc...)

Fixes #954, regression from #946

src/util/blockrules.ts
@@ -305,7 +305,6 @@ export class BlockRules {
       method: "PUT",
       headers: { "Content-Type": "text/html" },
       body,
-      dispatcher: getProxyDispatcher(putUrl.href),
     });
   }
 }

src/util/browser.ts
@@ -10,7 +10,7 @@ import path from "path";
 import { request } from "undici";
 
 import { formatErr, LogContext, logger } from "./logger.js";
-import { getSafeProxyString } from "./proxy.js";
+import { getFollowRedirectDispatcher, getSafeProxyString } from "./proxy.js";
 import { initStorage, S3StorageSync, UploadResult } from "./storage.js";
 
 import {
@@ -250,7 +250,9 @@ export class Browser {
         "browser",
       );
 
-      const resp = await request(profileRemoteSrc);
+      const resp = await request(profileRemoteSrc, {
+        dispatcher: getFollowRedirectDispatcher(),
+      });
 
       await pipeline(resp.body, fs.createWriteStream(profileLocalSrc));
     } else if (profileRemoteSrc && profileRemoteSrc.startsWith("@")) {

src/util/file_reader.ts
@@ -6,7 +6,7 @@ import util from "util";
 import { exec as execCallback } from "child_process";
 
 import { formatErr, logger } from "./logger.js";
-import { getProxyDispatcher } from "./proxy.js";
+import { getFollowRedirectDispatcher } from "./proxy.js";
 import { parseRecorderFlowJson } from "./flowbehavior.js";
 
 const exec = util.promisify(execCallback);
@@ -69,7 +69,6 @@ async function writeUrlContentsToFile(
   pathPrefix: string,
   pathDefaultExt: string,
   fetchNew = false,
-  useProxy = false,
 ): Promise<string> {
   const filename =
     path.basename(new URL(url).pathname) || "index." + pathDefaultExt;
@@ -90,7 +89,7 @@ async function writeUrlContentsToFile(
 
   try {
     const res = await request(url, {
-      dispatcher: useProxy ? getProxyDispatcher(url) : undefined,
+      dispatcher: getFollowRedirectDispatcher(),
     });
     if (res.statusCode !== 200) {
       throw new Error(`Invalid response, status: ${res.statusCode}`);
@@ -120,7 +119,6 @@ export async function collectOnlineSeedFile(
       url,
       "seeds-",
       ".txt",
-      false,
       false,
     );
     logger.info("Seed file downloaded", { url, path: filepath });
@@ -241,7 +239,6 @@ async function collectOnlineBehavior(
       "behaviors-",
       ".js",
       true,
-      false,
     );
     logger.info(
       "Custom behavior file downloaded",

src/util/proxy.ts
@@ -2,7 +2,7 @@ import net from "net";
 import child_process from "child_process";
 import fs from "fs";
 
-import { Agent, Dispatcher, ProxyAgent } from "undici";
+import { Agent, Dispatcher, interceptors, ProxyAgent } from "undici";
 import yaml from "js-yaml";
 
 import { logger } from "./logger.js";
@@ -17,12 +17,38 @@ const SSH_PROXY_LOCAL_PORT = 9722;
 
 const SSH_WAIT_TIMEOUT = 30000;
 
-//let proxyDispatcher: Dispatcher | undefined = undefined;
-
 type ProxyEntry = {
   proxyUrl: string;
   dispatcher: Dispatcher;
+  redirectDispatcher: Dispatcher;
 };
+
+// Opts for all requests
+const baseOpts: Agent.Options = {
+  headersTimeout: FETCH_HEADERS_TIMEOUT_SECS * 1000,
+
+  // allow HTTP/2 connections
+  allowH2: true,
+};
+
+// Opts for all archival content requests
+const contentAgentOpts: Agent.Options = {
+  ...baseOpts,
+
+  // ignore invalid SSL certs (matches browser settings)
+  connect: {
+    rejectUnauthorized: false,
+  },
+};
+
+// dispatcher for archival content without following redirects
+const contentDispatcher = new Agent(contentAgentOpts);
+
+// dispatcher for archival content with following redirects
+const contentRedirectDispatcher = addRedirectInterceptor(contentDispatcher);
+
+// dispatcher for following redirects, non-archival content, trust SSL
+const followRedirectDispatcher = addRedirectInterceptor(new Agent(baseOpts));
 
 export type ProxyServerConfig = {
   matchHosts?: Record<string, string>;
@@ -192,7 +218,7 @@ export async function initSingleProxy(
   detached: boolean,
   sshProxyPrivateKeyFile?: string,
   sshProxyKnownHostsFile?: string,
-): Promise<{ proxyUrl: string; dispatcher: Dispatcher }> {
+): Promise<ProxyEntry> {
   logger.debug("Initing proxy", {
     url: getSafeProxyString(proxyUrl),
     localPort,
@@ -210,23 +236,38 @@ export async function initSingleProxy(
     );
   }
 
-  const agentOpts: Agent.Options = {
-    headersTimeout: FETCH_HEADERS_TIMEOUT_SECS * 1000,
-  };
-
-  const dispatcher = createDispatcher(proxyUrl, agentOpts);
-  return { proxyUrl, dispatcher };
+  const dispatcher = createDispatcher(proxyUrl, contentAgentOpts);
+  const redirectDispatcher = addRedirectInterceptor(dispatcher);
+  return { proxyUrl, dispatcher, redirectDispatcher };
 }
 
-export function getProxyDispatcher(url: string) {
+export function addRedirectInterceptor(dispatcher: Dispatcher) {
+  // match fetch() max redirects if not doing manual redirects
+  // https://fetch.spec.whatwg.org/#http-redirect-fetch
+  const redirector = interceptors.redirect({ maxRedirections: 20 });
+  return dispatcher.compose(redirector);
+}
+
+export function getProxyDispatcher(url: string, withRedirect = true) {
   // find url match by regex first
-  for (const [rx, { dispatcher }] of proxyMap.entries()) {
+  for (const [rx, { dispatcher, redirectDispatcher }] of proxyMap.entries()) {
     if (rx && url.match(rx)) {
-      return dispatcher;
+      return withRedirect ? redirectDispatcher : dispatcher;
     }
   }
-  // if default proxy set, return default dispatcher, otherwise no dispatcher
-  return defaultProxyEntry ? defaultProxyEntry.dispatcher : undefined;
+
+  // if default proxy set, return dispatcher from default proxy, otherwise a default dispatcher
+  if (defaultProxyEntry) {
+    return withRedirect
+      ? defaultProxyEntry.redirectDispatcher
+      : defaultProxyEntry.dispatcher;
+  } else {
+    return withRedirect ? contentRedirectDispatcher : contentDispatcher;
+  }
+}
+
+export function getFollowRedirectDispatcher() {
+  return followRedirectDispatcher;
 }
 
 export function createDispatcher(

src/util/recorder.ts
@@ -8,7 +8,7 @@ import {
   isRedirectStatus,
 } from "./reqresp.js";
 
-import { Agent, Dispatcher, interceptors, request } from "undici";
+import { Dispatcher, request } from "undici";
 
 import {
   getCustomRewriter,
@@ -54,8 +54,6 @@ const RW_MIME_TYPES = [
 ];
 
 const encoder = new TextEncoder();
-
-const defaultAgent = new Agent();
 
 // =================================================================
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -1828,14 +1826,7 @@ class AsyncFetcher {
 
     const headers = reqresp.getRequestHeadersDict();
 
-    let dispatcher = getProxyDispatcher(url) || defaultAgent;
-
-    if (!this.manualRedirect) {
-      // match fetch() max redirects if not doing manual redirects
-      // https://fetch.spec.whatwg.org/#http-redirect-fetch
-      const redirector = interceptors.redirect({ maxRedirections: 20 });
-      dispatcher = dispatcher.compose(redirector);
-    }
+    let dispatcher = getProxyDispatcher(url, !this.manualRedirect);
 
     dispatcher = dispatcher.compose((dispatch) => {
       return (opts, handler) => {