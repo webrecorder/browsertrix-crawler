a19425f6a4e2d127c94c3f980d36d5fb9a8eb86f
Ilya Kreymer <ikreymer@gmail.com>
2026-02-02 23:02:35 -0800

    rate limit work: - detect 403, 429, 503 as possible rate limit, attempt to restart and not record

--- src/crawler.ts
+++ src/crawler.ts
@@ -64,8 +64,16 @@ import {
 import { Recorder } from "./util/recorder.js";
 import { SitemapReader } from "./util/sitemapper.js";
 import { ScopedSeed, parseSeeds } from "./util/seeds.js";
-import { WARCWriter, createWARCInfo, setWARCInfo } from "./util/warcwriter.js";
-import { isHTMLMime, isRedirectStatus } from "./util/reqresp.js";
+import {
+  WARCWriter,
+  createWARCInfo,
+  setWARCInfo,
+} from "./util/warcwriter.js";
+import {
+  isHTMLMime,
+  isRateLimitStatus,
+  isRedirectStatus,
+} from "./util/reqresp.js";
 import { initProxy } from "./util/proxy.js";
 import { initFlow, nextFlowStep } from "./util/flowbehavior.js";
 import { isDisallowedByRobots, setRobotsConfig } from "./util/robots.js";
@@ -1327,7 +1335,15 @@ self.__bx_behaviors.selectMainBehavior();
     }
     // if page loaded, considered page finished successfully
     // (even if behaviors timed out)
-    const { loadState, logDetails, depth, url, pageSkipped, noRetries } = data;
+    const {
+      loadState,
+      logDetails,
+      depth,
+      url,
+      pageSkipped,
+      pageRateLimited,
+      noRetries,
+    } = data;
 
     if (data.loadState >= LoadState.FULL_PAGE_LOADED) {
       await this.writePage(data);
@@ -1352,6 +1368,15 @@ self.__bx_behaviors.selectMainBehavior();
 
         if (this.healthChecker) {
           this.healthChecker.incError();
+        }
+        if (pageRateLimited) {
+          if (
+            (await this.crawlState.incRateLimited()) &&
+            this.params.restartsOnError
+          ) {
+            await this.serializeConfig();
+            await this.setStatusAndExit(ExitCodes.RateLimited, "interrupted");
+          }
         }
 
         if (retry < 0) {
@@ -2351,6 +2376,16 @@ self.__bx_behaviors.selectMainBehavior();
 
     const status = resp.status();
     data.status = status;
+
+    if (!isChromeError && isRateLimitStatus(status)) {
+      logger.warn(
+        "Page possibly rate limited, retrying",
+        { url, status, ...logDetails },
+        "pageStatus",
+      );
+      data.pageRateLimited = true;
+      throw new Error("logged");
+    }
 
     let failed = isChromeError;
 
