7b00b259de3b7bb336c6df66fa31ea8c17053c90
Ilya Kreymer <ikreymer@gmail.com>
2026-02-06 21:23:32 -0800

    fix dynamic page check to use correct pageUrl

--- src/crawler.ts
+++ src/crawler.ts
@@ -2227,9 +2227,7 @@ self.__bx_behaviors.selectMainBehavior();
 
     const urlNoHash = url.split("#")[0];
 
-    const pageUrl = page.url().split("#")[0];
-
-    const fullRefresh = urlNoHash === pageUrl;
+    const fullRefresh = urlNoHash === page.url().split("#")[0];
 
     try {
       if (!fullRefresh) {
@@ -2304,6 +2302,7 @@ self.__bx_behaviors.selectMainBehavior();
     }
 
     const respUrl = resp.url().split("#")[0];
+    const pageUrl = page.url();
     const isChromeError = pageUrl.startsWith("chrome-error://");
 
     if (
@@ -2384,11 +2383,7 @@ self.__bx_behaviors.selectMainBehavior();
       data.loadState = LoadState.FULL_PAGE_LOADED;
     }
 
-    if (
-      !isChromeError &&
-      pageUrl !== urlNoHash &&
-      !pageUrl.startsWith("about:blank")
-    ) {
+    if (!isChromeError && pageUrl !== urlNoHash) {
       if (!(await this.crawlState.addToUserSet(pageUrl))) {
         return this.pageSkipped(
           "Page dynamically changed to seen page, skipping",
