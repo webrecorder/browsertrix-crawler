33f095a2fb8b564c7f89b2ab518a0df0b2dacf3b
Ilya Kreymer <ikreymer@gmail.com>
2026-02-10 21:31:42 -0800

    post rebase fixes

--- src/util/state.ts
+++ src/util/state.ts
@@ -990,7 +990,7 @@ return inx;
   }
 
   async incRateLimited(rateLimitStatus: number) {
-    const key = this.key + ":rate";
+    const key = this.crawlId + ":rate";
     const RATE_LIMIT_TIME = 300;
     const RATE_LIMIT_MAX = 3;
 
