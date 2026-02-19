b9f912e2b7a2f6701221eeccb33e6281489839b8
Ilya Kreymer <ikreymer@users.noreply.github.com>
2026-02-19 10:48:36 -0800

    Support QA with deduped crawls (#977)
    deps: update to RWP 2.4.2 to support QA for deduped crawls, update
    wabac.js 2.25.4
    errors: always mark 404 as 0% match: if no replay resources returned but
    crawled resources exist, set text and screenshot match to 0
    
    ---------
    
    Co-authored-by: Tessa Walsh <tessa@bitarchivist.net>

--- src/replaycrawler.ts
+++ src/replaycrawler.ts
@@ -635,6 +635,13 @@ export class ReplayCrawler extends Crawler {
     resourceCounts.replayGood = replayGood;
     resourceCounts.replayBad = replayBad;
 
+    // If no resources replay, implies a 404 and set other matches to 0
+    if (replayGood === 0 && crawlGood + crawlBad > 0) {
+      logger.error("Replay Error, no resources replayed");
+      pageInfo.comparison.screenshotMatch = 0;
+      pageInfo.comparison.textMatch = 0;
+    }
+
     logger.info("Resource counts", { url, ...resourceCounts }, "replay");
   }
 
