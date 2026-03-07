4e585f7d28dd679f0705e7f6b5d2f717b80bf7ae
Ilya Kreymer <ikreymer@gmail.com>
2026-03-06 21:03:54 -0800

    add case

--- src/crawler.ts
+++ src/crawler.ts
@@ -669,6 +669,9 @@ export class Crawler {
             case InterruptReason.TimeLimit:
               exitCode = ExitCodes.TimeLimit;
               break;
+            case InterruptReason.RateLimited:
+              exitCode = ExitCodes.RateLimited;
+              break;
           }
         }
       }
