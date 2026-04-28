72349204dbcc413d887ad04d52c8457cada9201e
Tessa Walsh <tessa@bitarchivist.net>
2026-04-28 13:33:39 -0400

    Return tests to what they were in local branch before rebase

--- tests/scopes.test.ts
+++ tests/scopes.test.ts
@@ -407,6 +407,17 @@ scopeType: page
   expect((result as Exclude<typeof result, false>).isOOS).toBe(false);
 });
 
+test("allowHashUrls global with scopeType prefix", async () => {
+  const seeds = await getSeeds(`
+allowHashUrls: true
+seeds:
+  - url: https://example.com/
+`);
+
+  expect(seeds[0].scopeType).toEqual("prefix");
+  expect(seeds[0].allowHash).toEqual(true);
+});
+
 test("allowHash with scopeType prefix", async () => {
   const seeds = await getSeeds(`
 seeds:
@@ -418,29 +429,6 @@ scopeType: prefix
 
   expect(seeds[0].scopeType).toEqual("prefix");
   expect(seeds[0].allowHash).toEqual(true);
-
-  const result1 = seeds[0].isIncluded(
-    "/abc",
-    0,
-    0,
-    {},
-    false,
-    "https://example.com/",
-  );
-  expect(result1).not.toBe(false);
-  expect((result1 as Exclude<typeof result1, false>).url).toBe(
-    "https://example.com/abc",
-  );
-
-  const result2 = seeds[0].isIncluded(
-    "#abc",
-    0,
-    0,
-    {},
-    false,
-    "https://example.org/",
-  );
-  expect(result2).toBe(false);
 });
 
 test("hashtag relative URL included", async () => {
