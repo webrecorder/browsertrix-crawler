99fd62c5002b65bfc1414a9c13445f88166d6397
Ilya Kreymer <ikreymer@gmail.com>
2026-06-24 19:34:12 -0700

    optimizations and fixes for pending list: - call loadDirectPage() immediately instead of queuing, as async queue may grow large with unfinished pages (will revisit at a later time) - logging: print only first 10 entries in pending list, and total size of pending list, in case the list does grow large - state: whenever removing a url from pending list, also remove the <crawlid>:p:<url> lock, since the page is no longer pending, the lock key should be removed.

--- src/util/state.ts
+++ src/util/state.ts
@@ -936,6 +936,7 @@ if redis.call('sadd', KEYS[3], ARGV[1]) == 0 then
 end
 redis.call('zadd', KEYS[2], ARGV[2], ARGV[3]);
 redis.call('hdel', KEYS[1], ARGV[1]);
+redis.call('del', KEYS[1] .. ":" .. ARGV[1]);
 return 0;
 `,
     });
@@ -953,6 +954,7 @@ return 0;
       if json then
         local data = cjson.decode(json);
         redis.call('hdel', KEYS[2], data.url);
+        redis.call('del', KEYS[2] .. ":" .. data.url);
         redis.call('sadd', KEYS[3], data.url);
       end
       return 1;
@@ -1012,6 +1014,7 @@ if json then
   local retry = data['retry'] or 0;
 
   redis.call('hdel', KEYS[1], ARGV[1]);
+  redis.call('del', KEYS[1] .. ":" .. ARGV[1]);
 
   local maxRetries = tonumber(ARGV[2]);
 
@@ -1026,6 +1029,7 @@ if json then
     redis.call('lpush', KEYS[3], json);
   end
 end
+redis.call('del', KEYS[1] .. ":" .. ARGV[1]);
 return -1;
 
 `,
@@ -1100,6 +1104,7 @@ return inx;
     await this.redis.hdel(this.pkey, url);
 
     await this.redis.del(
+      this.pkey + ":" + url,
       `${this.crawlId}:rateLimited`,
       `${this.crawlId}:rateLimitedDirect`,
     );
@@ -1126,6 +1131,7 @@ return inx;
 
   async markExcluded(url: string) {
     await this.redis.hdel(this.pkey, url);
+    await this.redis.del(this.pkey + ":" + url);
 
     await this.redis.sadd(this.exKey, url);
   }
