webrecorder/browsertrix-crawler

commit 444cd3a5d5f5f89fbc924ddb630f7ff4a30ab4a1

quickfix: upload before deletion (#1109)
fix regression introduced in #1104 where the collection dir may be
deleted before profile upload is attempted:
- turn off auto-delete after every WACZ upload, as now mitigated by
better avoiding of accidental success exit codes, also fixed in #1104
- just in case, always move deletion after profile upload

src/crawler.ts
@@ -2133,6 +2133,8 @@ self.__bx_behaviors.selectMainBehavior();
       );
     }
 
+    let doDeleteDir = false;
+
     if (this.params.generateWACZ && generateFiles) {
       const wacz = await this.generateWACZ();
 
@@ -2144,16 +2146,7 @@ self.__bx_behaviors.selectMainBehavior();
         }
 
         if (this.storage && this.uploadAndDeleteLocal) {
-          await this.crawlState.setArchiveSize(0);
-
-          logger.info(
-            `Uploaded WACZ, deleting local data to free up space: ${this.collDir}`,
-          );
-          try {
-            fs.rmSync(this.collDir, { recursive: true, force: true });
-          } catch (e) {
-            logger.warn(`Unable to clear ${this.collDir} before exit`, e);
-          }
+          doDeleteDir = true;
         }
       }
     }
@@ -2180,6 +2173,21 @@ self.__bx_behaviors.selectMainBehavior();
       }
     }
 
+    // this should second-to-last
+    if (doDeleteDir) {
+      await this.crawlState.setArchiveSize(0);
+
+      logger.info(
+        `Uploaded WACZ, deleting local data to free up space: ${this.collDir}`,
+      );
+      try {
+        fs.rmSync(this.collDir, { recursive: true, force: true });
+      } catch (e) {
+        logger.warn(`Unable to clear ${this.collDir} before exit`, e);
+      }
+    }
+
+    // this should be last
     if (this.params.waitOnDone) {
       this.done = true;
       logger.info("All done, waiting for signal...");
@@ -2278,8 +2286,6 @@ self.__bx_behaviors.selectMainBehavior();
         const targetFilename = await this.crawlState.getWACZFilename();
 
         await this.storage.uploadCollWACZ(wacz, targetFilename, isFinished);
-
-        this.uploadAndDeleteLocal = true;
       }
       return wacz;
     } catch (e) {

commit 1ab000e7a1aac7c04dda5bd5d9d811351069c938

quickfix: follow up to regression caused by #1109: (#1115)
- if not 'final exit', (e.g. paused), the deletion code was not being
run before exit
- now check if doDelete is set and call deletion and clear data

src/crawler.ts
@@ -2168,6 +2168,7 @@ self.__bx_behaviors.selectMainBehavior();
         }
 
         if (this.storage && this.uploadAndDeleteLocal) {
+          // set to delete before exit, but after uploading profile, if saving profile
           doDeleteDir = true;
         }
       }
@@ -2175,6 +2176,10 @@ self.__bx_behaviors.selectMainBehavior();
 
     // from here, actions that should happen on final crawler exit (not temp interrupt)
     if (!this.finalExit) {
+      // if doDeleteDir is set, need to delete before exiting
+      if (doDeleteDir) {
+        await this.doDeleteDir();
+      }
       return;
     }
 
@@ -2184,6 +2189,7 @@ self.__bx_behaviors.selectMainBehavior();
       await this.crawlState.commitDedupeDone();
     }
 
+    // need to do this before deletion
     if (this.params.saveProfile && generateFiles) {
       const resource = await this.browser.saveProfile(
         this.params.saveProfile,
@@ -2197,16 +2203,7 @@ self.__bx_behaviors.selectMainBehavior();
 
     // this should second-to-last
     if (doDeleteDir) {
-      await this.crawlState.setArchiveSize(0);
-
-      logger.info(
-        `Uploaded WACZ, deleting local data to free up space: ${this.collDir}`,
-      );
-      try {
-        fs.rmSync(this.collDir, { recursive: true, force: true });
-      } catch (e) {
-        logger.warn(`Unable to clear ${this.collDir} before exit`, e);
-      }
+      await this.doDeleteDir();
     }
 
     // this should be last
@@ -2217,6 +2214,19 @@ self.__bx_behaviors.selectMainBehavior();
 
       // wait forever until signal
       await new Promise(() => {});
+    }
+  }
+
+  async doDeleteDir() {
+    await this.crawlState.setArchiveSize(0);
+
+    logger.info(
+      `Uploaded WACZ, deleting local data to free up space: ${this.collDir}`,
+    );
+    try {
+      fs.rmSync(this.collDir, { recursive: true, force: true });
+    } catch (e) {
+      logger.warn(`Unable to clear ${this.collDir} before exit`, e);
     }
   }
 