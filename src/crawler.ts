webrecorder/browsertrix-crawler

commit a648670ee3872c94f391e3576c7d95c12ee0df62

dedupe: don't add same WACZ file to per-crawl WACZ index (#1146)
- Only add WACZ file to `c:{crawlid}:wacz` index when setting a new WACZ
filename, not on every crawl start up
- Add it even if not using external redis, to keep logic simpler
- Fixes #1145

src/crawler.ts
@@ -1929,10 +1929,7 @@ self.__bx_behaviors.selectMainBehavior();
       this.params.generateWACZ &&
       (this.storage || this.isExternalDedupeStore)
     ) {
-      const filename = await this.crawlState.setWACZFilename();
-      if (this.isExternalDedupeStore) {
-        await this.crawlState.addSourceWACZForDedupe(filename);
-      }
+      await this.crawlState.setWACZFilename();
     }
     if (this.isExternalDedupeStore) {
       await this.crawlState.addCrawlForDedupe();

src/util/state.ts
@@ -1,7 +1,7 @@
 import { Redis, Result, Callback, type ChainableCommander } from "ioredis";
 import { v4 as uuidv4 } from "uuid";
 
-import { logger } from "./logger.js";
+import { formatErr, logger } from "./logger.js";
 
 import {
   MAX_DEPTH,
@@ -299,11 +299,21 @@ export class RedisDedupeIndex {
         index,
       );
       if (!waczdata) {
+        logger.warn(
+          "Invalid WACZ index, WARC-Refers-To-Container will not be set",
+          { crawlId, index },
+          "state",
+        );
         return "";
       }
       const { filename } = JSON.parse(waczdata);
       return filename;
-    } catch (_) {
+    } catch (e) {
+      logger.warn(
+        "Error getting WACZ index, WARC-Refers-To-Container will not be set",
+        { crawlId, index, ...formatErr(e) },
+        "state",
+      );
       return "";
     }
   }
@@ -854,8 +864,6 @@ export class RedisCrawlState extends RedisDedupeIndex {
 
   sitemapDoneKey: string;
 
-  waczFilename: string | null = null;
-
   includedCrawls: Set<string> = new Set<string>();
 
   rateLimitTTL: number;
@@ -1265,44 +1273,50 @@ return inx;
   }
 
   async setWACZFilename(): Promise<string> {
-    const filename = process.env.STORE_FILENAME || "@ts-@id.wacz";
-    this.waczFilename = interpolateFilename(filename, this.crawlId);
-    if (
-      !(await this.redis.hsetnx(
-        `${this.crawlId}:nextWacz`,
-        this.uid,
-        this.waczFilename,
-      ))
-    ) {
-      this.waczFilename = await this.redis.hget(
-        `${this.crawlId}:nextWacz`,
-        this.uid,
+    const filenameTemplate = process.env.STORE_FILENAME || "@ts-@id.wacz";
+    const crawlId = this.crawlId;
+    const filename = interpolateFilename(filenameTemplate, crawlId);
+    if (!(await this.redis.hsetnx(`${crawlId}:nextWacz`, this.uid, filename))) {
+      this.dedupeCurrFilename =
+        (await this.redis.hget(`${crawlId}:nextWacz`, this.uid)) || "";
+
+      this.dedupeKeyIndex = Number(
+        (await this.redis.hget(`${crawlId}:nextWaczIndex`, this.uid)) || 0,
       );
+
       logger.debug(
         "Keeping WACZ Filename",
-        { filename: this.waczFilename },
+        { filename, index: this.dedupeKeyIndex },
         "state",
       );
     } else {
+      await this.addSourceWACZForDedupe(filename);
+
+      await this.redis.hset(
+        `${crawlId}:nextWaczIndex`,
+        this.uid,
+        this.dedupeKeyIndex,
+      );
+
       logger.debug(
         "Using New WACZ Filename",
-        { filename: this.waczFilename },
+        { filename, index: this.dedupeKeyIndex },
         "state",
       );
     }
-    return this.waczFilename!;
+    return filename;
   }
 
   async getWACZFilename(): Promise<string> {
-    if (!this.waczFilename) {
+    if (!this.dedupeCurrFilename) {
       return await this.setWACZFilename();
     }
-    return this.waczFilename;
+    return this.dedupeCurrFilename;
   }
 
   async clearWACZFilename(): Promise<void> {
     await this.redis.hdel(`${this.crawlId}:nextWacz`, this.uid);
-    this.waczFilename = null;
+    this.dedupeCurrFilename = "";
 
     await this.redis.del(`${this.uid}:duperef`);
   }

commit b07caa5d239bc4a9537d641f501856fd3b31b06d

dedupe: ensure WACZ filename is not cleared before it is set in per-wacz dedupe key (#1150)
- fixes regression introduced in #1146
- tests: refactor dedupe-basic test to allow accessing redis more easily
- tests: add test to verify `c:<crawl-id>:wacz key` is as expected

src/crawler.ts
@@ -2161,11 +2161,11 @@ self.__bx_behaviors.selectMainBehavior();
       const wacz = await this.generateWACZ();
 
       if (wacz) {
-        await this.crawlState.clearWACZFilename();
-
         if (this.isExternalDedupeStore) {
           await this.crawlState.updateDedupeSourceWACZ(wacz);
         }
+
+        await this.crawlState.clearWACZFilename();
 
         if (this.storage && this.uploadAndDeleteLocal) {
           // set to delete before exit, but after uploading profile, if saving profile

tests/dedupe-basic.test.ts
@@ -88,7 +88,7 @@ function loadDataPackageRelated(name: string) {
   return dataPackageJSON.relation;
 }
 
-async function redisGetHash(key: string, db = 0) {
+async function initRedis(db = 0) {
   const redis = new Redis(`redis://127.0.0.1:37379/${db}`, {
     lazyConnect: true,
     retryStrategy: () => null,
@@ -98,16 +98,16 @@ async function redisGetHash(key: string, db = 0) {
 
   await redis.connect();
 
-  return await redis.hgetall(key);
+  return redis;
 }
 
 async function checkSizeStats(
+  redis: Redis,
   numUniq: number,
   key: string,
-  db: number,
   minSize: number,
 ) {
-  const result = await redisGetHash(key, db);
+  const result = await redis.hgetall(key);
   console.log(result);
   expect(numUniq).toBeLessThan(Number(result.totalUrls));
 
@@ -162,7 +162,9 @@ test("check revisit records written on duplicate crawl, same collection, no wacz
 
   numResponses = response;
 
-  await checkSizeStats(numResponses, "allcounts", 0, 77000);
+  const redis = await initRedis(0);
+
+  await checkSizeStats(redis, numResponses, "allcounts", 77000);
 });
 
 test("dedupe same collection, with wacz, no external waczs referenced", async () => {
@@ -173,6 +175,16 @@ test("dedupe same collection, with wacz, no external waczs referenced", async ()
   const related = loadDataPackageRelated(collName);
 
   expect(related).toBe(undefined);
+
+  const redis = await initRedis(0);
+
+  const waczList = await redis.lrange(`c:${collName}:wacz`, 0, -1);
+  expect(waczList.length).toBe(1);
+
+  const parts = JSON.parse(waczList[0]);
+  expect(parts.filename).toBe("dedupe-test-same-coll.wacz");
+  expect(parts.hash).toBeDefined();
+  expect(parts.size).toBeDefined();
 });
 
 test("check revisit records written on duplicate crawl, different collections, with wacz", async () => {
@@ -219,7 +231,9 @@ test("check revisit records written on duplicate crawl, different collections, w
 
   numResponses = response;
 
-  sizeSaved = await checkSizeStats(numResponses, "allcounts", 1, 48400000);
+  const redis = await initRedis(1);
+
+  sizeSaved = await checkSizeStats(redis, numResponses, "allcounts", 48400000);
 });
 
 test("import dupe index, orig then revisits, from single wacz", async () => {
@@ -227,14 +241,7 @@ test("import dupe index, orig then revisits, from single wacz", async () => {
     `docker run -v $PWD/test-crawls:/crawls --network=dedupe webrecorder/browsertrix-crawler indexer --sourceUrl /crawls/collections/dedupe-test-orig/dedupe-test-orig.wacz --sourceCrawlId dedupe-test-orig --redisDedupeUrl redis://dedupe-redis:6379/2`,
   );
 
-  const redis = new Redis("redis://127.0.0.1:37379/2", {
-    lazyConnect: true,
-    retryStrategy: () => null,
-  });
-
-  redis.options.maxRetriesPerRequest = 50;
-
-  await redis.connect();
+  await initRedis(2);
 });
 
 test("verify new crawl against imported dupe index has same dupes as dedupe against original", async () => {
@@ -260,7 +267,9 @@ test("verify new crawl against imported dupe index has same dupes as dedupe agai
   // matches same number of revisits as original
   expect(revisit).toBe(numResponses);
 
-  await checkSizeStats(numResponses, "allcounts", 2, 48400000);
+  const redis = await initRedis(2);
+
+  await checkSizeStats(redis, numResponses, "allcounts", 48400000);
 });
 
 test("import dupe index from json, reverse, revisits than orig, from wacz", async () => {
@@ -294,21 +303,14 @@ test("import dupe index from json, reverse, revisits than orig, from wacz", asyn
     `docker run -v $PWD/test-crawls:/crawls --network=dedupe webrecorder/browsertrix-crawler indexer --sourceUrl /crawls/collections/dedupe-test-dupe/import-1.json --redisDedupeUrl redis://dedupe-redis:6379/3`,
   );
 
-  const redis = new Redis("redis://127.0.0.1:37379/3", {
-    lazyConnect: true,
-    retryStrategy: () => null,
-  });
-
-  redis.options.maxRetriesPerRequest = 50;
-
-  await redis.connect();
+  const redis = await initRedis(3);
 
   expect(await redis.hlen("alldupes")).toBe(numResponses);
 
   const sizeSavedImport = await checkSizeStats(
+    redis,
     numResponses,
     "allcounts",
-    3,
     48400000,
   );
 