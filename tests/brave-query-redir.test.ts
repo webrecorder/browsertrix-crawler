68666aacd4f1b3cde004d3d7882bf59fc25fd30e
Ilya Kreymer <ikreymer@gmail.com>
2026-08-20 19:03:01 -0700

    tests: disable brave query redirect, pending changing in brave (see: brave/brave-browser#58320)

--- tests/brave-query-redir.test.ts
+++ tests/brave-query-redir.test.ts
@@ -1,7 +1,9 @@
 import fs from "fs";
 import { ExecException, execSync } from "child_process";
 
-test("check that gclid query URL is automatically redirected to remove it", async () => {
+// disable pending brave issue
+
+xtest("check that gclid query URL is automatically redirected to remove it", async () => {
   try {
     execSync(
       "docker run --rm  -v $PWD/test-crawls:/crawls -i webrecorder/browsertrix-crawler crawl --url 'https://old.webrecorder.net/about?gclid=abc' --collection test-brave-redir --behaviors \"\" --limit 1 --generateCDX",
